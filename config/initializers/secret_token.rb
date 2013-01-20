59b32e6a86e1efd5b816c3e88206d722fcde2829
Philipp Sokolov <philipp@futurecrazy.co.uk>
2013-01-20 12:59:24 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstMApp::Application.config.secret_token = '108a800e2a39f27dc9e6603335580ff5bd0cd11834882350b8d3268e84d1ab9c44d1d2caefc33116195a9ec54f50ab784f54eb4e8352da5d52766cfb2aa72905'
